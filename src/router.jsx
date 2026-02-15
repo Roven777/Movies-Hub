7123189f2661b2e3337976a0b0afcfd5b8ae51d8
Roven777 <kroven2994@gmail.com>
2026-02-16 03:35:47 +0530

    For Depolyment

--- src/router.jsx
+++ src/router.jsx
@@ -1,14 +1,14 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import StartPage from "./pages/StartPage";
 import HomePage from "./pages/HomePage";
 import MovieDetail from "./pages/MovieDetail";
-import SearchPage from "./components/SearchPage"; // ✅ ADD
+import SearchPage from "./components/SearchPage";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   { path: "/", element: <StartPage /> },
   { path: "/home", element: <HomePage /> },
   { path: "/movie/:id", element: <MovieDetail /> },
-  { path: "/search", element: <SearchPage /> }, // ✅ ADD
+  { path: "/search", element: <SearchPage /> },
 ]);
 
 export default router;
